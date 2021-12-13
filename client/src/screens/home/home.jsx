5db6219f156f8379e3535537b49201b712b3a959
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-12-13 14:01:00 -0800

    home

--- client/src/screens/home/home.jsx
+++ client/src/screens/home/home.jsx
@@ -5,7 +5,18 @@ const Home = () => {
   return (
     <div className="home">
       <div className="menu">
-        <div className="menu__title">Cookie Menu</div>
+        <div className="menu__title">
+          <h2>Cookie Menu</h2>
+        </div>
+        <ol>
+          <li>
+            <div>
+              <h3>Chocolate Chip</h3>
+              <p>lorem ipsum dolor sit amet</p>
+            </div>
+            
+          </li>
+        </ol>
       </div>
     </div>
   );
