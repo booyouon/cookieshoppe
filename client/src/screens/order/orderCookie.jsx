71c265b663ee1a1643b5920c90e2a260d8d52c3f
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-12-16 00:08:06 -0800

    order cookie

--- client/src/screens/order/orderCookie.jsx
+++ client/src/screens/order/orderCookie.jsx
@@ -4,8 +4,8 @@ const OrderCookie = ({ cookies }) => {
   console.log(cookies);
   return (
     <div>
-      {cookies.map((cookie) => (
-        <div>
+      {cookies.map((cookie, idx) => (
+        <div className="order__cookie" key={idx}>
           <img src={cookie.img_url} alt="" />
           <h3>{cookie.name}</h3>
         </div>
