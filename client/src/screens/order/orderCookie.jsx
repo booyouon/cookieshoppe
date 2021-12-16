4593c19b39bac61f04ea2d18b9c38cc859d5b863
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-12-16 00:12:52 -0800

    cookie

--- client/src/screens/order/orderCookie.jsx
+++ client/src/screens/order/orderCookie.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const OrderCookie = ({ cookies }) => {
   console.log(cookies);
   return (
-    <div>
+    <div className="order__cookieContainer">
       {cookies.map((cookie, idx) => (
         <div className="order__cookie" key={idx}>
-          <img src={cookie.img_url} alt="" />
+          <img src={cookie.img_url} alt={cookie.name} />
           <h3>{cookie.name}</h3>
         </div>
       ))}
