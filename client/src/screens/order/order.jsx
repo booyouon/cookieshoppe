27092745586b0d833488747c7e370aee47c4e11d
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-12-15 18:34:42 -0800

    order

--- client/src/screens/order/order.jsx
+++ client/src/screens/order/order.jsx
@@ -3,11 +3,13 @@ import "./order.css";
 import OrderMethod from "./orderInitial";
 import OrderMain from "./orderMain";
 import OrderCookie from "./orderCookie";
+import { useState } from "react";
 
 const Order = ({ cookies }) => {
+  const [delivery, setDelivery] = useState("");
   return (
     <div>
-      <OrderMethod />
+      <OrderMethod setDelivery={setDelivery} />
       <OrderMain />
       <OrderCookie cookies={cookies} />
     </div>
